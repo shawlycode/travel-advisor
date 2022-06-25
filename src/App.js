bc152ec88bc4275ac82d6bf3c1db445dcd1bf1fb
Shawlycode <kutinaorlando@gmail.com>
2022-06-25 01:46:36 +0000

    added and utilized states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
 import Header from "./components/Header/Header";
+import { getPlacesData } from "./api";
 
 const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [bounds, setBounds] = useState(null);
+  const [coordinates, setCoordinates] = useState({});
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+  useEffect(() => {
+    getPlacesData().then((data) => {
+      // console.log(coordinates, bounds);
+      setPlaces(data);
+    });
+  }, [coordinates, bounds]);
+
   return (
     <>
       <CssBaseline />
@@ -14,7 +32,11 @@ const App = () => {
           <List />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+          />
         </Grid>
       </Grid>
     </>
